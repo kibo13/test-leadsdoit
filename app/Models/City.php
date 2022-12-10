c038068b394421d26f263c3d8bbb02061c12b011
kibo13 <kimboris1310@gmail.com>
2022-12-10 15:38:56 +0600

    feat(cities, forecasts): add tables and models for cities and forecasts

---
+++ app/Models/City.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class City extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Attributes that must be converted to a date
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at'
+    ];
+}
---
+++ app/Models/Forecast.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Forecast extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'city_id',
+        'data',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+}
